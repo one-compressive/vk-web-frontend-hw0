1db032fdf319e723095891c339a8e5e644647236
Ann <oparina.anna2006@yandex.ru>
2026-02-14 17:13:03 +0300

    перезалив

---
+++ source/fibonacciGenerator.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/**
+ * Функция, которая принимает одно число n и возвращает генератор, генерирующий первые n чисел последовательности Фибоначчи.
+ * Если n меньше или равно 0, генератор не должен генерировать никаких значений.
+ * @param {Number} n
+ *
+ * @example
+ * // 1, затем 2, затем 3
+ * const gen = fibonacciGenerator(3);
+ * for(let value of gen) {
+ *   alert(value);
+ * }
+ *
+ * @returns {object}
+ */
+
+function* fibonacciGenerator(n) {
+    if (n <= 0) return;
+
+    if (n == 1) {
+        yield 0;
+        return;
+    }
+
+    let first = 0;
+    let second = 1;
+    for (let i = 0; i < n; ++i) {
+        yield first;
+        [first, second] = [second, first + second];
+    }
+}
+
+const gen = fibonacciGenerator(3);
+for(let value of gen) {
+  alert(value); // 1, затем 2, затем 3
+}
--- test/fibonacciGenerator.js
+++ test/fibonacciGenerator.js
@@ -6,7 +6,7 @@ QUnit.module("Тестируем функцию fibonacciGenerator", function() 
 
         assert.deepEqual([...fibGen], [0], "Должно быть сгенерировано только первое число Фибоначчи.");
     });
-    
+
     QUnit.test("Правильно генерирует 5 первых чисел Фибоначчи", function(assert) {
         const fibGen = fibonacciGenerator(5);
 
@@ -17,5 +17,23 @@ QUnit.module("Тестируем функцию fibonacciGenerator", function() 
         const fibGen = fibonacciGenerator(-5);
 
         assert.deepEqual([...fibGen], [], "Генерация отрицательного числа должна вернуть пустой массив.");
+    });
+
+    QUnit.test("Корректность первых 7 чисел", function(assert) {
+        assert.deepEqual(
+            [...fibonacciGenerator(7)],
+            [0, 1, 1, 2, 3, 5, 8],
+            "Должны быть сгенерированы первые 7 чисел."
+        );
+    });
+
+    QUnit.test("n = 0 возвращает пустую последовательность", function(assert) {
+        const fibGen = fibonacciGenerator(0);
+
+        assert.deepEqual(
+            [...fibGen],
+            [],
+            "При n = 0 генератор не должен генерировать значения."
+        );
     });
 });
